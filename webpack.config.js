bb15ededa3b14db056c36ed7c9fb43903cfa4a0a
Wikiverse <damdinbazar@o365.skku.edu>
2021-11-23 04:27:24 +0900

    Implemented Sign in and Sign up

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    entry: {
-        auth: './auth/auth.js',
-        rdb: './rdb/rdb.js',
-        storage: './storage/storage.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    devtool: 'source-map'
-}
+  entry: {
+    auth: './auth/auth.js',
+    rdb: './rdb/rdb.js',
+    storage: './storage/storage.js',
+    signin: './auth-test/signin.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  devtool: 'source-map',
+};
