75d470fcbbd3838d9c884737243285b07ea90f9d
Wikiverse <damdinbazar@o365.skku.edu>
2021-11-24 05:50:01 +0900

    Revert "delete console.log email"
    This reverts commit 7ecee964c94efa86ba2e2a0c59a2356d6e998adb.

--- auth-test/signin.js
+++ auth-test/signin.js
@@ -1,31 +1,34 @@
-import { app } from "../src/firebase";
-import * as auth from "../src/firebase-auth";
+import { app } from '../src/firebase';
+import * as auth from '../src/firebase-auth';
 
 const login = () => {
-    const signinBtn = document.getElementById("signin-btn");
-    signinBtn.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+  const signinBtn = document.getElementById('signin-btn');
+  signinBtn.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
   Signing in`;
-    const log = document.querySelector(".login-message");
-    log.innerHTML = "";
-    auth.authSignIn(
-        app,
-        document.getElementById("signin-email").value,
-        document.getElementById("signin-pw").value
-    ).then((resolve) => {
-        if (!resolve) {
-            log.innerHTML = "Either email address or username is wrong.";
-        } else {
-            document.location.href = "../profile/profile.html";
-        }
-        signinBtn.innerHTML = "Sign in";
+  const log = document.querySelector('.login-message');
+  log.innerHTML = '';
+  auth
+    .authSignIn(
+      app,
+      document.getElementById('signin-email').value,
+      document.getElementById('signin-pw').value
+    )
+    .then((resolve) => {
+      if (!resolve) {
+        log.innerHTML = 'Either email address or username is wrong.';
+      } else {
+        document.location.href = '../profile/profile.html';
+      }
+      signinBtn.innerHTML = 'Sign in';
     });
 };
 const signup = () => {
-    const username = document.getElementById("username").value;
-    const domain = document.getElementById("domain").value;
-    const email = username + "@" + domain;
-    auth.authSignUp(app, email, document.getElementById("signup-pw").value);
+  const username = document.getElementById('username').value;
+  const domain = document.getElementById('domain').value;
+  const email = username + '@' + domain;
+  console.log(email);
+  auth.authSignUp(app, email, document.getElementById('signup-pw').value);
 };
-document.getElementById("signin-btn").addEventListener("click", login);
+document.getElementById('signin-btn').addEventListener('click', login);
 
-document.getElementById("signup-btn").addEventListener("click", signup);
+document.getElementById('signup-btn').addEventListener('click', signup);
