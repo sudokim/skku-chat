173d1469482ef51495005aba831f9dc8d708ef80
Wikiverse <damdinbazar@o365.skku.edu>
2021-11-24 14:10:38 +0900

    updated styling

--- src/firebase-auth.js
+++ src/firebase-auth.js
@@ -1,10 +1,10 @@
 import {
-    createUserWithEmailAndPassword,
-    deleteUser,
-    getAuth,
-    signInWithEmailAndPassword,
-    signOut,
-} from 'firebase/auth'
+  createUserWithEmailAndPassword,
+  deleteUser,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
 
 /**
  * Sign up for a new account
@@ -13,28 +13,29 @@ import {
  * @param password_ Password of the user
  */
 export function authSignUp(app_, email_, password_) {
-    // Email validity check
-    const skku_domain = ['skku.edu', 'g.skku.edu', 'o365.skku.edu']
-    const re =
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-            .exec(email_)
+  // Email validity check
+  const skku_domain = ['skku.edu', 'g.skku.edu', 'o365.skku.edu'];
+  const re =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.exec(
+      email_
+    );
 
-    if (re !== null && skku_domain.includes(re[0].split('@')[1])) {
-        // Valid email used, continue
-        const auth = getAuth(app_)
+  if (re !== null && skku_domain.includes(re[0].split('@')[1])) {
+    // Valid email used, continue
+    const auth = getAuth(app_);
 
-        createUserWithEmailAndPassword(auth, email_, password_)
-            .then((uc) => {
-                alert('User created\n' + uc.user.email)
-                // TODO: Email verification
-            })
-            .catch((err) => {
-                alert('Error during user creation\n(' + err.code + ') ' + err.message)
-            })
-    } else {
-        // Invalid email used
-        alert('Invalid email')
-    }
+    createUserWithEmailAndPassword(auth, email_, password_)
+      .then((uc) => {
+        alert('User created\n' + uc.user.email);
+        // TODO: Email verification
+      })
+      .catch((err) => {
+        alert('Error during user creation\n(' + err.code + ') ' + err.message);
+      });
+  } else {
+    // Invalid email used
+    alert('Invalid email');
+  }
 }
 
 /**
@@ -45,18 +46,18 @@ export function authSignUp(app_, email_, password_) {
  * @returns {Promise<boolean>} Returns a promise for a boolean value of whether the sign in was successful or not
  */
 export async function authSignIn(app_, email_, password_) {
-    const auth = getAuth(app_)
-    let success
-    await signInWithEmailAndPassword(auth, email_, password_)
-        .then((uc) => {
-            alert('Signed in\n' + uc.user.email)
-            success = true
-        })
-        .catch((err) => {
-            alert('Error during sign in\n(' + err.code + ') ' + err.message)
-            success = false
-        })
-    return success
+  const auth = getAuth(app_);
+  let success;
+  await signInWithEmailAndPassword(auth, email_, password_)
+    .then((uc) => {
+      alert('Signed in\n' + uc.user.email);
+      success = true;
+    })
+    .catch((err) => {
+      alert('Error during sign in\n(' + err.code + ') ' + err.message);
+      success = false;
+    });
+  return success;
 }
 
 /**
@@ -65,18 +66,18 @@ export async function authSignIn(app_, email_, password_) {
  * @returns {boolean} Boolean value of whether the user is signed in
  */
 export function authSignInStatus(app_) {
-    const auth = getAuth(app_)
-    const user = auth.currentUser
+  const auth = getAuth(app_);
+  const user = auth.currentUser;
 
-    if (user) {
-        // User logged in
-        alert('User ' + user.email + '\nwith UID ' + user.uid + '\nis logged in')
-        return true
-    } else {
-        // User not logged in
-        alert('User not logged in')
-        return false
-    }
+  if (user) {
+    // User logged in
+    alert('User ' + user.email + '\nwith UID ' + user.uid + '\nis logged in');
+    return true;
+  } else {
+    // User not logged in
+    alert('User not logged in');
+    return false;
+  }
 }
 
 /**
@@ -85,25 +86,25 @@ export function authSignInStatus(app_) {
  * @returns {Promise<boolean>} Returns a promise for a boolean value of whether the sign out was successful or not
  */
 export async function authSignOut(app_) {
-    const auth = getAuth(app_)
-    const user = auth.currentUser
+  const auth = getAuth(app_);
+  const user = auth.currentUser;
 
-    if (user) {
-        // User logged in -> sign out
-        signOut(auth)
-            .then(() => {
-                alert('Sign out successful')
-                return true
-            })
-            .catch((err) => {
-                alert('Error during sign out\n(' + err.code + ') ' + err.message)
-                return false
-            })
-    } else {
-        // User not logged in
-        alert('User not logged in')
-        return false
-    }
+  if (user) {
+    // User logged in -> sign out
+    signOut(auth)
+      .then(() => {
+        alert('Sign out successful');
+        return true;
+      })
+      .catch((err) => {
+        alert('Error during sign out\n(' + err.code + ') ' + err.message);
+        return false;
+      });
+  } else {
+    // User not logged in
+    alert('User not logged in');
+    return false;
+  }
 }
 
 /**
@@ -112,23 +113,23 @@ export async function authSignOut(app_) {
  * @returns {Promise<boolean>} Returns a promise for a boolean value of whether deleting the user was successful or not
  */
 export async function authDeleteUser(app_) {
-    const auth = getAuth(app_)
-    const user = auth.currentUser
+  const auth = getAuth(app_);
+  const user = auth.currentUser;
 
-    if (user) {
-        // User logged in -> delete
-        deleteUser(user)
-            .then(() => {
-                alert('User delete successful')
-                return true
-            })
-            .catch((err) => {
-                alert('Error during user deletion\n(' + err.code + ') ' + err.message)
-                return false
-            })
-    } else {
-        // User not logged in
-        alert('User not logged in')
-        return false
-    }
+  if (user) {
+    // User logged in -> delete
+    deleteUser(user)
+      .then(() => {
+        alert('User delete successful');
+        return true;
+      })
+      .catch((err) => {
+        alert('Error during user deletion\n(' + err.code + ') ' + err.message);
+        return false;
+      });
+  } else {
+    // User not logged in
+    alert('User not logged in');
+    return false;
+  }
 }
--- src/firebase-storage.js
+++ src/firebase-storage.js
@@ -1,11 +1,4 @@
-import {
-    getStorage,
-    ref,
-    list,
-    uploadBytes,
-    getDownloadURL,
-    deleteObject
-} from 'firebase/storage'
+import { getStorage, ref, list, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
 
 /**
  * Upload a file to the storage
@@ -15,19 +8,19 @@ import {
  * @returns {Promise<boolean>} Returns a promise for a boolean value of whether the upload was successful
  */
 export async function storageUpload(app_, src, dest) {
-    // Storage and reference
-    const storage = getStorage(app_)
-    const uploadRef = ref(storage, dest)
+  // Storage and reference
+  const storage = getStorage(app_);
+  const uploadRef = ref(storage, dest);
 
-    uploadBytes(uploadRef, src)
-        .then(r => {
-            alert('Uploaded file to ' + r.ref.fullPath)
-            return true
-        })
-        .catch(err => {
-            alert('Error during uploading file\n(' + err.code + ') ' + err.message)
-            return false
-        })
+  uploadBytes(uploadRef, src)
+    .then((r) => {
+      alert('Uploaded file to ' + r.ref.fullPath);
+      return true;
+    })
+    .catch((err) => {
+      alert('Error during uploading file\n(' + err.code + ') ' + err.message);
+      return false;
+    });
 }
 
 /**
@@ -37,19 +30,19 @@ export async function storageUpload(app_, src, dest) {
  * @returns {Promise<boolean>} Returns a promise for a boolean value of whether deleting the file was successful
  */
 export async function storageDelete(app_, file) {
-    // Storage and reference
-    const storage = getStorage(app_)
-    const fileRef = ref(storage, file)
+  // Storage and reference
+  const storage = getStorage(app_);
+  const fileRef = ref(storage, file);
 
-    deleteObject(fileRef)
-        .then(() => {
-            alert('Deleted file at ' + file)
-            return true
-        })
-        .catch(err => {
-            alert('Error during deleting file\n(' + err.code + ') ' + err.message)
-            return false
-        })
+  deleteObject(fileRef)
+    .then(() => {
+      alert('Deleted file at ' + file);
+      return true;
+    })
+    .catch((err) => {
+      alert('Error during deleting file\n(' + err.code + ') ' + err.message);
+      return false;
+    });
 }
 
 /**
@@ -59,19 +52,19 @@ export async function storageDelete(app_, file) {
  * @returns {Promise<String> | null} A promise of a string representation of the list of the files if the directory exists, or null if the directory does not exist
  */
 export async function storageView(app_, dir) {
-    // Storage and reference
-    const storage = getStorage(app_)
-    const dirRef = ref(storage, dir)
+  // Storage and reference
+  const storage = getStorage(app_);
+  const dirRef = ref(storage, dir);
 
-    list(dirRef)
-        .then(r => {
-            alert(r.items.toString())
-            return r.items.toString()
-        })
-        .catch(err => {
-            alert('Error during list directory\n(' + err.code + ') ' + err.message)
-            return null
-        })
+  list(dirRef)
+    .then((r) => {
+      alert(r.items.toString());
+      return r.items.toString();
+    })
+    .catch((err) => {
+      alert('Error during list directory\n(' + err.code + ') ' + err.message);
+      return null;
+    });
 }
 
 /**
@@ -81,16 +74,16 @@ export async function storageView(app_, dir) {
  * @returns {Promise<String>} A promise of an URL
  */
 export async function storageGetURL(app_, file) {
-    // Storage and reference
-    const storage = getStorage(app_)
-    const fileRef = ref(storage, file)
+  // Storage and reference
+  const storage = getStorage(app_);
+  const fileRef = ref(storage, file);
 
-    getDownloadURL(fileRef)
-        .then(r => {
-            alert(r)
-            return r
-        })
-        .catch(err => {
-            alert('Error during getting URL\n(' + err.code + ') ' + err.message)
-        })
+  getDownloadURL(fileRef)
+    .then((r) => {
+      alert(r);
+      return r;
+    })
+    .catch((err) => {
+      alert('Error during getting URL\n(' + err.code + ') ' + err.message);
+    });
 }
